66c403f9c826fe54e7c1ec7a98afdbaff572860f
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-10 16:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign(driver, {[key]:value});
 }
+
+function deleteFromDriverByKey(driver, key) {
+  temp =  Object.assign({}, driver);
+  delete temp[key];
+  return temp;
+}
